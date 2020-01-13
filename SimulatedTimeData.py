5ff0908d18b5ad1daddf6160c581570b439729a4
Dizzled <dustin.lagrone@gmail.com>
2020-01-12 19:59:21 -0800

    First Commit

--- SimulatedTimeData.py
+++ SimulatedTimeData.py
@@ -1,47 +1,54 @@
 #!/usr/bin/env python
 import tkinter as tk
 from tkinter import filedialog
-from pandas import DataFrame
+import pandas as pd
+import random
 
-global df
-
-def export(dataFile):
+# Export out to csv format
+def export():
 
     exportFilePath = filedialog.asksaveasfile(defaultextension='.csv')
-    dataFile.to_csv(exportFilePath, index = None, header=True)
+    df.to_csv(exportFilePath, index = None, header=True, line_terminator='\n', sep=',')
 
 # Function to output sensor time
 def sensorTime():
-    sensorTime = 0
-    sensor = []
+    list = []
     for i in range(0, 3001):
-        sensorTime += .02
-        sensor.append(sensorTime)
-
-    return sensor
+        k = i * 0.02
+        list.append(k)
+    df2 = pd.DataFrame({'dataValidTime':list})
+    return df2
 
 # Function to output processor time
 def processorTime():
     sensorStartTime = 1000
     processor = []
-
+    #Random Time Step Based on Min Max values for given intervals
     for i in range(0, 3001):
-
+        sensorStartTime += random.uniform(.0001, .069321)
         processor.append(sensorStartTime)
 
     return processor
 
+    return processor
+
+# Generate csv File dataframe
 def createFile():
-
-    #Create Headers for file dataFrame
+    global df
     root = tk.Tk()
+    pd.set_option('display.max_rows',3001)
+    df = pd.DataFrame(columns=['dataValidTime'])
 
-    df = DataFrame(columns=['dataValidTime', 'dataReceiveTime_System'])
-    df.append(sensorTime())
+    addedSensor = sensorTime()
+    addedProcessor = processorTime()
+
+    df = df.append(addedSensor, ignore_index=True, sort=False)
+    df.insert(1, 'dataReceiveTime_System', addedProcessor, True)
+
     canvas = tk.Canvas(root, width = 300, height = 300, bg = 'grey', relief = 'raised')
     canvas.pack()
 
-    saveButton = tk.Button(text='Generate Simulation Data', command=export(df), bg='green', fg='white', font=('helvetica', 12, 'bold'))
+    saveButton = tk.Button(text='Generate Simulation Data', command=export(), bg='green', fg='white', font=('helvetica', 12, 'bold'))
 
     canvas.create_window(150, 150, window=saveButton)
     root.mainloop()
